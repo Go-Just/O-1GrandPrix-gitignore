0ad0c9217ab986823668cdc1c085bef96d1f8650
ichiyajou0125 <120617535+ichiyajou0125@users.noreply.github.com>
2025-05-13 22:19:03 +0900

    Unityファイルのアップロード

---
+++ O-1グランプリ/root/Assets/scripts/Code/AllCharacters.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+using UnityEngine.AddressableAssets;
+
+public class AllCharacters : MonoBehaviour
+{
+    public List<Characters> AllCharacter;
+
+    async void Start()
+    {
+        AllCharacter = new List<Characters>();
+        
+        await CharacterLoad();
+
+        foreach(var Chara in AllCharacter){
+            Debug.Log(Chara.Charactername);
+        }
+    }
+
+    public async Task CharacterLoad(){
+        var handle = Addressables.LoadAssetsAsync<Characters>("geininn", character =>
+        {
+            AllCharacter.Add(character);
+        });
+        await handle.Task;
+    }
+}

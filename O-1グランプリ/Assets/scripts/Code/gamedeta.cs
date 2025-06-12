246a75d9cbca29de0aad9e638335cbbd1a093d1a
knHLRr352 <jm79.n10@gmail.com>
2025-06-12 16:36:21 +0900

    リスト作ったよ～

---
+++ O-1グランプリ/Assets/scripts/Code/gamedeta.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "gamedeta",menuName = "Data/gamedeta")]
+public class gamedeta : ScriptableObject
+{
+    public List<Characters> CharactersList;
+}
+
